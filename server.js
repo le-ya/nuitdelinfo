0ea996c89dcda40584eb3fbfc0194471c61296fe
Ozenski1 <enzodc6688@gmail.com>
2020-12-06 01:48:59 +0100

    petite maj

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-
 var express = require('express');
 var app = express();
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
+app.set('views', './views');
 
-// use res.render to load up an ejs view file
-
-// index page
-app.use(express.static(__dirname + "/public", {
-    index: false,
-    immutable: true,
-    cacheControl: true,
-    maxAge: "30d"
-}));
-
-app.use(express.static(__dirname + "/js", {
-    index: false,
-    immutable: true,
-    cacheControl: true,
-    maxAge: "30d"
-}));
-
-app.use(express.static(__dirname + "/img", {
-    index: false,
-    immutable: true,
-    cacheControl: true,
-    maxAge: "30d"
-}));
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/images'))
 
 app.get('/', function(req, res) {
     res.render('pages/index.ejs');
 });
 
-// about page
+app.get('/contact', function(req, res) {
+    res.render('pages/contact');
+});
+
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-app.get('/contact', function(req, res) {
-    res.render('pages/contact');
-});
-app.listen(8085);
+app.listen(8080);
 console.log('8080 is the magic port');
